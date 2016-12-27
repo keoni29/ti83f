9280a431f1350b2b42dc9e3356bf380a9d2f1965
Koen van Vliet <koen9410@gmail.com>
2016-12-27 12:01:03 +0100

    Refactoring

--- to8xv.py
+++ to8xv.py
@@ -13,44 +13,61 @@ def bytes_pad(b, size):
 	else:
 		return b + bytearray(size - len(b))
 
+class AppVar:
+	TI83F_SIGNATURE = b'\x2A\x2A\x54\x49\x38\x33\x46\x2A\x1A\x0A\x00'
 
-TI83F_SIGNATURE = b'\x2A\x2A\x54\x49\x38\x33\x46\x2A\x1A\x0A\x00'
-TI83F_COMMENT = b'AppVariable file'
+	def __init__(self, comment = b'AppVariable file'):
+		self.TI83F_COMMENT = bytes_pad(comment, 42)
+		self.vars = []
 
-VAR_START = b'\x0D\x00'
-VAR_TYPEID = b'\x15'
-VAR_NAME = b'HCMT'
-VAR_VERSION = b'\x00'
-VAR_FLAG = b'\x80'	# \x80 for archived. \x00 otherwise
-varHeaderSize = 19
+	def __bytes__(self):
+		data = b''
+		for var in self.vars:
+			data += bytes(var)
 
-# Pad certain fields (TODO refactor)
-TI83F_COMMENT = bytes_pad(TI83F_COMMENT, 42)
-VAR_NAME = bytes_pad(VAR_NAME, 8)
+		header = self.TI83F_SIGNATURE + self.TI83F_COMMENT + int2b16(len(data))
+
+		# Lower 16 bits of sum of all bytes in data segment
+		checksum = int2b16(sum(data))
+
+		return header + data + checksum
+
+	def add(self, var):
+		self.vars.append(var)
+
+class Var:
+	VAR_START = b'\x0D\x00'
+	VAR_TYPEID = b'\x15'
+	VAR_VERSION = b'\x00'
+
+	def __init__(self, name, archived=False):
+		self.name = bytes_pad(name, 8)
+		if archived:
+			self.flag = b'\x80'
+		else:
+			self.flag = b'\x00'
+
+	def __bytes__(self):
+		dataSize = len(self.data)
+		varSize = dataSize + 2		# 2 bytes for variable header
+
+		header = self.VAR_START + int2b16(varSize) + self.VAR_TYPEID + self.name + \
+			self.VAR_VERSION	+ self.flag + int2b16(varSize) + int2b16(dataSize)
+
+		return header + self.data
+
 
 fname_src = "HCMT.tmp"
 # TODO base output filename on input filename if no
 # output name is specified!
 fname_dst = "HCMT.8xv"
 
-src = open(fname_src, 'rb')
-dst = open(fname_dst, 'wb')
-VAR_DATA = src.read(-1)
+appv = AppVar()
+var = Var(b'HCMT', archived=True)
 
-dataSize = len(VAR_DATA)
-varSize = dataSize + 2		# 2 bytes for variable header
+with open(fname_src, 'rb') as src:
+	with open(fname_dst, 'wb') as dst:
+		var.data = src.read()
+		appv.add(var)
 
-APPV_HEADER = TI83F_SIGNATURE + TI83F_COMMENT + int2b16(dataSize + varHeaderSize)
-
-VAR_HEADER = VAR_START + int2b16(varSize) + VAR_TYPEID + VAR_NAME + \
-	VAR_VERSION	+ VAR_FLAG + int2b16(varSize) + int2b16(dataSize)
-
-# Lower 16 bits of sum of all bytes in data segment and variable header combined
-VAR_SUM = sum(VAR_HEADER) + sum(VAR_DATA)
-VAR_CHECKSUM = int2b16(VAR_SUM)
-print("Checksum =", hex(VAR_SUM))
-
-dst.write(APPV_HEADER + VAR_HEADER + VAR_DATA + VAR_CHECKSUM);
-
-src.close()
-dst.close()
+		dst.write(bytes(appv));
