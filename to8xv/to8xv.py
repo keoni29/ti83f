75f584d56b575c6418608ab88e98f041399fbed2
Koen van Vliet <8by8mail@gmail.com>
2018-05-13 12:33:33 +0200

    Added documentation

--- to8xv/to8xv.py
+++ to8xv/to8xv.py
@@ -1,28 +1,32 @@
-#!/usr/bin/env python3
 """	to8xv: Convert any file to appVar
 	(Python version) """
 
-import argparse
-import os
-import ti83f
+def main():
+    import argparse
+    import os
+    import to8xv.ti83f
 
-parser = argparse.ArgumentParser()
-parser.add_argument("filename", type=str)
-parser.add_argument("-o", "--output", type=str)
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=str)
+    parser.add_argument("-o", "--output", type=str)
+    args = parser.parse_args()
 
-fname_src = args.filename
-if args.output:
-	fname_dst = args.output
-else:
-	fname_dst = os.path.splitext(fname_src)[0] + '.8xv'
+    fname_src = args.filename
+    if args.output:
+        fname_dst = args.output
+    else:
+        fname_dst = os.path.splitext(fname_src)[0] + '.8xv'
 
-appv = ti83f.AppVar()
-var = ti83f.Var(b'HCMT', archived=True)
+    appv = to8xv.ti83f.AppVar()
+    variable = to8xv.ti83f.Variable(b'HCMT', archived=True)
 
-with open(fname_src, 'rb') as src:
-	with open(fname_dst, 'wb') as dst:
-		var.data = src.read()
-		appv.add(var)
+    with open(fname_src, 'rb') as src:
+        with open(fname_dst, 'wb') as dst:
+            variable.data = src.read()
+            appv.add(variable)
 
-		dst.write(bytes(appv));
+            dst.write(bytes(appv))
+
+if __name__ == "__main__":
+    main()
+    
